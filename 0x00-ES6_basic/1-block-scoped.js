10a1bd791eb8f24664e09fb49d96b81e1659d4ba
tommyokoyo <okoyotommy@gmail.com>
2023-08-22 23:47:13 +0300

    Block Scope - eslint disable check

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,11 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable */
     const task = true;
     const task2 = false;
   }
+    /* eslint-enable */
 
   return [task, task2];
 }
