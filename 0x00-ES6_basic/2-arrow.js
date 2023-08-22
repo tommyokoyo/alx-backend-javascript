9c1b5a13dc063fbe4888cb650afce1e429b0349d
tommyokoyo <okoyotommy@gmail.com>
2023-08-23 00:12:27 +0300

    Arrow functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/prefer-default-export
+export function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
