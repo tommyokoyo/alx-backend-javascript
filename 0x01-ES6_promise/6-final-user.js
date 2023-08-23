2cab949e2ea2e4381fe0c0535c1ec4644cbaa946
tommyokoyo <okoyotommy@gmail.com>
2023-08-24 00:42:14 +0300

    Handle multiple promises - edit

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => ({
+      status: values.status,
+      value: values.value,
+    }));
 }
