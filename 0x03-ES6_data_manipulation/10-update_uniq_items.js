afb2095143160ac958360636878d84d2e300ccb6
tommyokoyo <okoyotommy@gmail.com>
2023-09-01 15:45:15 +0300

    More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,16 +1,9 @@
 function updateUniqueItems(inputMap) {
   if (!(inputMap instanceof Map)) throw new Error('Cannot process');
-
-  const updatedMap = new Map();
-
-  for (const [item, quantity] of inputMap.entries()) {
-    if (quantity === 1) {
-      updatedMap.set(item, 100);
-    } else {
-      updatedMap.set(item, quantity);
-    }
+  for (const [key] of inputMap) {
+    if (inputMap.get(key) === 1) inputMap.set(key, 100);
   }
-  return updatedMap;
+  return inputMap;
 }
 
 export default updateUniqueItems;
