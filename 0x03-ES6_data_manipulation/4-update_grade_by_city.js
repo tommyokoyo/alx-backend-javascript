0d7c3d14c3a8051a1ab84c7907587254c9e3ac67
tommyokoyo <okoyotommy@gmail.com>
2023-09-01 12:41:39 +0300

    Combine - updated logic

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,18 @@
 function updateStudentGradeByCity(students, location, newGrades) {
-  const newfilteredStudents = students.filter((student) => student.location);
-  const mappedStud = newfilteredStudents.map((student) => {
-    const studentWithNewGrades = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+  const newGradedStudents = students.filter((student) => student.location === location)
+    .map((student) => {
+      const studentWithNewGrades = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
 
-    const newGradesToDisplay = studentWithNewGrades ? studentWithNewGrades.grade : 'N/A';
+      const newGradesToDisplay = studentWithNewGrades ? studentWithNewGrades.grade : 'N/A';
 
-    return {
-      id: student.id,
-      firstName: student.firstName,
-      location: student.location,
-      grade: newGradesToDisplay,
-    };
-  });
-  return mappedStud;
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: newGradesToDisplay,
+      };
+    });
+  return newGradedStudents;
 }
 
 export default updateStudentGradeByCity;
