d7115c006cadc4712af1007bb7e3d6cdcaf56179
tommyokoyo <okoyotommy@gmail.com>
2023-09-01 08:09:03 +0300

    A Course, Getters, and Setters eee

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,11 @@
 export default class HolbertonCourse {
   constructor(name = '', length = 0, students = ['']) {
-    if (typeof name !== 'string') {throw TypeError('Name must be a string')}
-    else if (typeof length !== 'number') {throw TypeError('Length must be an number')}
-    else if (students.constructor !== Array && students.every((val) => typeof val === 'string')) {
+    if (typeof name !== 'string') { throw TypeError('Name must be a string'); } else if (typeof length !== 'number') { throw TypeError('Length must be an number'); } else if (students.constructor !== Array && students.every((val) => typeof val === 'string')) {
       throw TypeError('students must be an array of strings');
     } else {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+      this._name = name;
+      this._length = length;
+      this._students = students;
     }
   }
 
@@ -24,7 +22,7 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof newName !== 'string') throw TypeError('Name must be a string');
+    if (typeof newName !== 'string') throw TypeError('Name must be a String');
     this._name = newName;
   }
 
