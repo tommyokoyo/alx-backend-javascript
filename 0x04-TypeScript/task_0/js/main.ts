5d0c590d78c8cb374bed26b7f032617e6af37ec0
tommyokoyo <okoyotommy@gmail.com>
2023-09-02 08:49:21 +0300

    Using Process stdin

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,45 @@
+interface Student {
+    firstName: String;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 20,
+    location: "New York"
+};
+
+const student2: Student = {
+    firstName: "Alice",
+    lastName: "Smith",
+    age: 22,
+    location: "Los Angeles"
+};
+
+const studentsList = [student1, student2];
+
+function renderTable() {
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+
+    studentsList.forEach((student) => {
+        const row = document.createElement('tr');
+        const firstNameCell = document.createElement('td');
+        const locationCell = document.createElement('td');
+        // @ts-ignore
+        firstNameCell.textContent = student.firstName;
+        locationCell.textContent = student.location;
+
+        row.appendChild(firstNameCell);
+        row.appendChild(locationCell);
+        tbody.appendChild(row);
+    });
+
+    table.appendChild(tbody);
+    document.body.appendChild(table);
+}
+
+renderTable();
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+    const username = process.stdin.read();
+
+    if (username) {
+        process.stdout.write(`Your name is: ${username}`);
+    }
+});
+
+process.stdin.on('close', () => {
+    process.stdout.write('This important software is now closing');
+});
