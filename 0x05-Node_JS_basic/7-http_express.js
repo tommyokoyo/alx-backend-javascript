ad18f6b68ea1dfe95ecea01c85fa24f08703e43d
tommyokoyo <okoyotommy@gmail.com>
2023-09-05 02:22:43 +0300

    Create a more complex HTTP server using Express - Eslint

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const countStudents = require("./3-read_file_async");
+const countStudents = require('./3-read_file_async');
+
 const app = express();
 const port = 1245;
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 const database = args[0];
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-    const msg = 'This is the list of our students\n';
-    try {
-        const students = await countStudents(database);
-        res.send(`${msg}${students.join('\n')}`);
-    } catch (error) {
-        res.end(error.message);
-    }
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(database);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.end(error.message);
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
 
 module.exports = app;
