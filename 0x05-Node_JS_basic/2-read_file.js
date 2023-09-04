172b3e61bcf6666efc4fc5986450fb2c9478c9da
tommyokoyo <okoyotommy@gmail.com>
2023-09-04 21:58:42 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,41 @@
-function countStudents() {}
+const fs = require('fs');
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf-8');
+        const extracted_lines = data.split('\n').filter((line) => line.trim() !== '');
+        const persons = extracted_lines.filter((person) => person).map((item) => item.split(','));
+        const fieldCounts = {};
+        const fields_in_obj = {};
+
+        persons.forEach((line) => {
+            const field_0 = line[0];
+            const field_3 = line[3];
+
+            if (field_3 !== 'field') {
+                if (!fieldCounts[field_3]) {
+                    fieldCounts[field_3] = 0;
+                    fields_in_obj[field_3] = [];
+                }
+                fields_in_obj[field_3].push(field_0);
+                fieldCounts[field_3] += 1;
+            }
+        });
+
+        const totalStudents = extracted_lines.length;
+
+        console.log(`Number of Students: ${totalStudents}`);
+        for (const key in fieldCounts) {
+            console.log(`Number of students in ${key}: ${fieldCounts[key]}. List: ${fields_in_obj[key]}`)
+        }
+
+
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('Cannot load the database: File not found');
+        } else {
+            console.error('An error occurred while reading the database: ', error.message);
+        }
+    }
+}
+
+module.exports = countStudents;
---
+++ 0x05-Node_JS_basic/scratch.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('database.csv', 'utf-8');
+const lines = data.split('\n').filter((line) => line.trim() !== '');
+const persons = lines.filter((person) => person).map((item) => item.split(','));
+console.log(persons[0][0]);
+
+
