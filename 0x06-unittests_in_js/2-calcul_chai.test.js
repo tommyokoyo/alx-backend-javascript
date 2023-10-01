3f3d666658d0d8a21ceb953d838edee2632747d3
Tommyokoyo <okoyotommy@gmail.com>
2023-10-01 17:53:47 +0300

    Basic test using Chai assertion library - correction

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,23 +2,23 @@ const expect = require('chai').expect;
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-    it('should correctly perform SUM operation', () => {
+    it('Test for SUM operation', () => {
         expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
 
-    it('should correctly perform SUBTRACT operation', () => {
+    it('Test for SUBTRACT operation', () => {
         expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
 
-    it('should correctly perform DIVIDE operation', () => {
+    it('Test for DIVIDE operation', () => {
         expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
 
-    it('should handle division by 0 and return Error', () => {
+    it('Test for division by 0 and return Error', () => {
         expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
 
-    it('should throw an error for invalid operation type', () => {
+    it('Test for error for invalid operation type', () => {
         expect(() => {
             calculateNumber('INVALID', 1.4, 4.5);
         }).to.throw(/Invalid type. Type must be SUM, SUBTRACT, or DIVIDE/);
