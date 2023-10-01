cd431c8d8c38b9bc30f171cb777c7cba267010ad
Tommyokoyo <okoyotommy@gmail.com>
2023-10-01 17:28:08 +0300

    Test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,10 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-    it('Should return correct sum', () => {
+    it('Check if numbers round', () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
     });
-    it('Should return correct sum', () => {
+    it('Check if numbers round', () => {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
     it('Should return correct sum', () => {
