65a12abb26caf0baa26baf9124f8b817e9b39b5b
Tommyokoyo <okoyotommy@gmail.com>
2023-10-01 17:59:28 +0300

    Basic test using Chai assertion library - correction

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,25 +2,99 @@ const expect = require('chai').expect;
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
-    it('Test for SUM operation', () => {
-        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    describe('type == "SUM"', () => {
+        it('Test for SUM operation', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+
+        it('Test for SUBTRACT operation', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+
+        it('Test for DIVIDE operation', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        });
+
+        it('Test for division by 0 and return Error', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+
+        it('Test for error for invalid operation type', () => {
+            expect(() => {
+                calculateNumber('INVALID', 1.4, 4.5);
+            }).to.throw(/Invalid type. Type must be SUM, SUBTRACT, or DIVIDE/);
+        });
     });
 
-    it('Test for SUBTRACT operation', () => {
-        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    describe('type == "SUBTRACT"', () => {
+        it('Test for SUM operation', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+
+        it('Test for SUBTRACT operation', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+
+        it('Test for DIVIDE operation', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        });
+
+        it('Test for division by 0 and return Error', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+
+        it('Test for error for invalid operation type', () => {
+            expect(() => {
+                calculateNumber('INVALID', 1.4, 4.5);
+            }).to.throw(/Invalid type. Type must be SUM, SUBTRACT, or DIVIDE/);
+        });
     });
 
-    it('Test for DIVIDE operation', () => {
-        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    describe('type == "DIVIDE"', () => {
+        it('Test for SUM operation', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
+
+        it('Test for SUBTRACT operation', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+
+        it('Test for DIVIDE operation', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        });
+
+        it('Test for division by 0 and return Error', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+
+        it('Test for error for invalid operation type', () => {
+            expect(() => {
+                calculateNumber('INVALID', 1.4, 4.5);
+            }).to.throw(/Invalid type. Type must be SUM, SUBTRACT, or DIVIDE/);
+        });
     });
 
-    it('Test for division by 0 and return Error', () => {
-        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-    });
+    describe('type == "DIVIDE AND RETURN"', () => {
+        it('Test for SUM operation', () => {
+            expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+        });
 
-    it('Test for error for invalid operation type', () => {
-        expect(() => {
-            calculateNumber('INVALID', 1.4, 4.5);
-        }).to.throw(/Invalid type. Type must be SUM, SUBTRACT, or DIVIDE/);
+        it('Test for SUBTRACT operation', () => {
+            expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+        });
+
+        it('Test for DIVIDE operation', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+        });
+
+        it('Test for division by 0 and return Error', () => {
+            expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+
+        it('Test for error for invalid operation type', () => {
+            expect(() => {
+                calculateNumber('INVALID', 1.4, 4.5);
+            }).to.throw(/Invalid type. Type must be SUM, SUBTRACT, or DIVIDE/);
+        });
     });
 });
